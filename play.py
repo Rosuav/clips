42815f5f96fc6b592aec8f39962ef9bc7cd9af12
Chris Angelico <rosuav@gmail.com>
2019-04-26 06:25:38 +1000

    Allow pinpoint removal where needed

--- play.py
+++ play.py
@@ -1,4 +1,5 @@
 import json
+import os
 import subprocess
 import sys
 
@@ -6,11 +7,17 @@ dir = ""
 if len(sys.argv) > 1: dir = sys.argv[1] + "/"
 
 while True:
-	try: fn = input("Enter .json file name: ").strip()
+	try: fn = input("Enter .json file name: ")
 	except EOFError: break
 	if not fn: break
-	with open(dir + fn) as f: meta = json.load(f)
+	if fn == "rm":
+		# Delete the last-named file (will bomb if no last file)
+		subprocess.check_call(["git", "rm", base + ".json"])
+		os.unlink(base + ".mp4")
+		continue
+	base = dir + fn.strip().replace(".json", "")
+	with open(base + ".json") as f: meta = json.load(f)
 	print()
 	print("Clip taken by", meta["curator"]["display_name"], "at", meta["created_at"])
 	print(meta["title"])
-	subprocess.check_call(["google-chrome", dir + fn.replace(".json", "") + ".mp4"])
+	subprocess.check_call(["google-chrome", base + ".mp4"])
